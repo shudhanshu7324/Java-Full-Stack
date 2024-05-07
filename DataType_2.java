cf4f1429159dd555176532c21f8ec8ea10ff3444
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-05-08 00:47:18 +0530

    datatypes

---
+++ DataType_2.java
@@ -0,0 +1,19 @@
+public class DataType_2 {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1+num2;
+        System.out.println(result);
+
+
+        float f = 6.367f;  // write f at end
+        long l = 848348l; // explicitly write L at end
+        double d = 56.67;
+        char x = 'a';
+        boolean z = true;
+        System.out.println(f);
+        System.out.println(z);
+
+
+    }
+}
